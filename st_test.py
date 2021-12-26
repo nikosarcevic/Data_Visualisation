4236eecd0bd89a26f53d330fe8eb947059463ae3
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-26 20:34:54 +0000

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -1,6 +1,8 @@
 # %%
 import numpy as np
 import streamlit as st
+import yaml
+from scipy import integrate
 
 def calculate_distance_modulus(d):
     '''
@@ -19,12 +21,44 @@ def calculate_distance_modulus(d):
     
     return mu
 
+def E_z(z, ΩM=constants['matter-density'], ΩDE=constants['DE-density'], 
+        ΩR=constants['rad-density'], w0=constants['w0'], wa=constants['wa']):
+    """
+    Method to compute the adimensional Hubble rate in the w0waCDm cosmology
+    """
+    ΩK = 1-ΩM-ΩDE-ΩR
+    return np.sqrt(ΩM*(1+z)**3+ΩR*(1+z)**4+ΩDE*(1+z)**(3*(1+w0+wa))*np.exp(-3*wa*z/(1+z))+ΩK*(1+z)**2)
+
+def comoving_distance(z, H0=constants['Hubble0'], ΩM=constants['matter-density'], 
+                      ΩDE=constants['DE-density'], ΩR=constants['rad-density'], 
+                      w0=constants['w0'], wa=constants['wa']):
+    """
+    Method to compute the comoving distance
+    """
+    integrand = lambda x: 1/E_z(x, ΩM, ΩDE, ΩR, w0, wa)
+    if isinstance(z, float):
+        result, _ = integrate.quad(integrand, 0, z)
+    elif isinstance(z, np.ndarray):
+        result = np.vectorize(lambda x: integrate.quad(integrand, 0, x)[0])(z)
+    else:
+        raise TypeError(f'Expected "Union[float, np.ndarray]", got {type(z)}')
+    c0 = constants['speed-of-light']
+    return c0/H0*result
 
 
-value = st.text_input('Distance') 
+distance_value = st.text_input('Distance') 
+if distance_value :
+    st.write('The distance modulus is:', calculate_distance_modulus(float(distance_value )), '[no units]')
+    
+ 
+z_value = st.text_input('Redshift')
 
+if z_value:
+    st.write('comoving distance is:', comoving_distance(float(z_value)), 'blah')
+    
 
-if value:
-    st.write('The distance modulus is:', calculate_distance_modulus(float(value)), '[no units]')
+                 
+                        
+                        
 
 
