94698fba1626b66bd71322a067e9f3e606e33216
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-26 19:31:15 +0000

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -1,10 +1,5 @@
 # %%
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import plotly.graph_objects as go
 import streamlit as st
 
 def calculate_distance_modulus(d):
