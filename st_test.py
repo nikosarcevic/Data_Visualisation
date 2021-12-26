ecb68494dbeeed40ac9cc5c93f87634a7fb4f636
Nikolina Sarcevic <32773944+nikosarcevic@users.noreply.github.com>
2021-12-26 21:23:44 +0000

    Update st_test.py

--- st_test.py
+++ st_test.py
@@ -15,6 +15,43 @@ with name:
 
 st.sidebar.write(" ")
 
+def main():
+    """
+    Register pages to Explore and Fit:
+        page_introduction - contains page with images and brief explanations
+        page_explore - contains various functions that allows exploration of
+                        continuous distribution; plotting class and export
+        page_fit - contains various functions that allows user to upload
+                    their data as a .csv file and fit a distribution to data.
+    """
+
+    pages = {
+        "Introduction": page_introduction,
+        "Explore distributions": page_explore,
+        "Fit distributions": page_fit,
+    }
+
+    st.sidebar.title("Main options")
+
+    # Radio buttons to select desired option
+    page = st.sidebar.radio("Select:", tuple(pages.keys()))
+                                
+    # Display the selected page with the session state
+    pages[page]()
+
+    # Write About
+    st.sidebar.header("About")
+    st.sidebar.warning(
+            """
+            Distribution Analyser app is created and maintained by 
+            **Robert Dzudzar**. If you like this app please star its
+            [**GitHub**](https://github.com/rdzudzar/DistributionAnalyser)
+            repo, share it and feel free to open an issue if you find a bug 
+            or if you want some additional features.
+            """
+    )
+
+
 
 def calculate_distance_modulus(d):
     '''
